5c234aa53f7b494004f3ba8a668ade3ff220a754
N <n@tioi.network>
2024-02-12 00:53:13 +0800

    chore: Add google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from "next/app";
 import "../styles/index.css";
+import { Bangers, Dekko } from 'next/font/google'
+
+const bangers = Bangers({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-bangers',
+})
+
+const dekko = Dekko({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-dekko',
+})
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
