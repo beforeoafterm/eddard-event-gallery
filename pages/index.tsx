3d391585da667a1b944c050cbf44f5e5e6792d33
N <ronneil@athenago.com>
2024-02-25 16:57:57 +0800

    feat: Image Categories
    Adds category buttons on the Home page to filter out images in view.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,37 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useRef } from "react";
-import Bridge from "../components/Icons/Bridge";
-import Logo from "../components/Icons/Logo";
-import Modal from "../components/Modal";
-import cloudinary from "../utils/cloudinary";
-import getBase64ImageUrl from "../utils/generateBlurPlaceholder";
-import type { ImageProps } from "../utils/types";
-import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect, useMemo, useRef, useState } from "react"
+import Modal from "../components/Modal"
+import cloudinary from "../utils/cloudinary"
+import getBase64ImageUrl from "../utils/generateBlurPlaceholder"
+import type { ImageProps } from "../utils/types"
+import { useLastViewedPhoto } from "../utils/useLastViewedPhoto"
 
-const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
-  const router = useRouter();
-  const { photoId } = router.query;
-  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
+const Home: NextPage = ({ categories, images }: {
+  categories: Array<string>,
+  images: ImageProps[]
+}) => {
+  const router = useRouter()
+  const { photoId } = router.query
+  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto()
+  const [selectedCategory, setSelectedCategory] = useState<string>(categories[0])
 
-  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
+  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null)
+
+  const categoryImages = useMemo(() => {
+    return images.filter((image) => image.public_id.includes(selectedCategory))
+  }, [selectedCategory])
 
   useEffect(() => {
     // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
     if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
-      setLastViewedPhoto(null);
+      lastViewedPhotoRef.current.scrollIntoView({ block: "center" })
+      setLastViewedPhoto(null)
     }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
 
   return (
     <>
@@ -45,15 +51,15 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           <Modal
             images={images}
             onClose={() => {
-              setLastViewedPhoto(photoId);
+              setLastViewedPhoto(photoId)
             }}
           />
         )}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden border-8 border-black bg-sky-600/90 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
-            <div className="absolute inset-0 flex items-center justify-center opacity-20">
+          <div className="after:content relative mb-5 flex flex-col items-center justify-end gap-4 overflow-hidden border-8 border-black bg-sky-600/90 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+            <div className="absolute inset-0 flex items-center justify-center opacity-80 -z-10">
               <span className="flex max-h-full max-w-full items-center justify-center">
-                <Image src='/ned-logo.svg' layout='fill' alt='Kirk Superhero Logo' />
+                <Image src='/ned-logo.svg' width={400} height={400} alt='Kirk Superhero Logo' />
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
@@ -61,12 +67,25 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             <h1 className="mt-8 mb-4 text-2xl font-bold font-display tracking-widest">
               Kirk's 7th Birthday Superhero Bash Photos
             </h1>
+            <div className="">
+              {categories.map((category) => (
+                <button
+                  className={[
+                    "categoryButton",
+                    selectedCategory === category ? 'categoryButton___selected' : '',
+                  ].join(' ')}
+                  onClick={() => setSelectedCategory(category)}
+                >
+                  {category.replace(/_/g, ' ')}
+                </button>
+              ))}
+            </div>
             <p className="max-w-[40ch] text-lg text-white/80 sm:max-w-[32ch] font-default">
               Our superheroes and civilians got together in Heroes Headquarters for
               a fun-filled action-packed celebration!
             </p>
           </div>
-          {images.map(({ id, public_id, format, blurDataUrl }) => (
+          {categoryImages.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
               key={id}
               href={`/?photoId=${id}`}
@@ -76,7 +95,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
             >
               <Image
-                alt="Next.js Conf photo"
+                alt="Kirk Eddard's 7th Birthday photo"
                 className="transform border-8 border-black brightness-90 transition will-change-auto group-hover:brightness-110"
                 style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
@@ -101,66 +120,60 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           className="font-semibold hover:text-white"
           rel="noreferrer"
         >
-          Josh Edelson
+          Pam Nicolas
         </a>
-        ,{" "}
-        <a
-          href="https://www.newrevmedia.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Jenny Morgan
-        </a>
-        , and{" "}
-        <a
-          href="https://www.garysextonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Gary Sexton
-        </a>{" "}
+        {" "}
         for the pictures.
       </footer>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
 
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
     .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
     .sort_by("public_id", "desc")
     .max_results(400)
-    .execute();
-  let reducedResults: ImageProps[] = [];
+    .execute()
+  let images: ImageProps[] = []
 
-  let i = 0;
+  let i = 0
+  const categories: Array<string> = []
   for (let result of results.resources) {
-    reducedResults.push({
+
+    // Get the category name from the public ID
+    const categoryName = result.public_id.split('/')[1].split('-')[1]
+
+    // Store all categories
+    if (!categories.includes(categoryName)) {
+      categories.push(categoryName)
+    }
+
+    images.push({
       id: i,
       height: result.height,
       width: result.width,
       public_id: result.public_id,
       format: result.format,
-    });
-    i++;
+    })
+    i++
   }
 
   const blurImagePromises = results.resources.map((image: ImageProps) => {
-    return getBase64ImageUrl(image);
-  });
-  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+    return getBase64ImageUrl(image)
+  })
+  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises)
 
-  for (let i = 0; i < reducedResults.length; i++) {
-    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+  for (let i = 0; i < images.length; i++) {
+    images[i].blurDataUrl = imagesWithBlurDataUrls[i]
   }
 
   return {
     props: {
-      images: reducedResults,
-    },
-  };
+      categories,
+      images
+    }
+  }
 }
